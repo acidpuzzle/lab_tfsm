1bda02b8c08090d346439be35ee32141a4efee29
Pavlov <acidpuzzle@mail.ru>
2022-09-02 15:51:51 +0300

    edit dict generator

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import textfsm
 
 
 def parse_output_to_dict(template, command_output):
-    with open(template) as template:
-        fsm = textfsm.TextFSM(template)
-        result = fsm.ParseTextToDicts(command_output)
-    return result
+    with open(template) as tmpl:
+        parser = textfsm.TextFSM(tmpl)
+        header = parser.header
+        result = parser.ParseText(command_output)
+    return [dict(zip(header, line)) for line in result]
 
 
 if __name__ == "__main__":
